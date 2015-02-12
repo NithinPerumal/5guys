b19e8f6b3f2ce26efc11fe185f3a1ecbe7cf168a
Tim Smith <timothys740@gmail.com>
2015-02-12 10:02:19 -0500

    More changes to topbar

--- Project Code/src/topBar.java
+++ Project Code/src/topBar.java
@@ -13,11 +13,18 @@ public class topBar {
 	public topBar(){
 		menubar = new JMenuBar();
 		//menubar.setBackground(Color.BLUE);
-		JMenu menu = new JMenu("hello");
+		JMenu menu = new JMenu("Home");
+		JMenu menu2 = new JMenu("Trip Planner");
+		JMenu menu3 = new JMenu("Search");
 		JMenuItem item = new JMenuItem("hello");
+		JMenuItem itemm3 = new JMenuItem("City");
+		JMenuItem itemm32 = new JMenuItem("Path");
 		menu.add(item);
-		menu.add(item);
+		menu3.add(itemm3);
+		menu3.add(itemm32);
 		menubar.add(menu);
+		menubar.add(menu2);
+		menubar.add(menu3);
 		menubar.setPreferredSize(new Dimension(1000, 20));
 	}
 
