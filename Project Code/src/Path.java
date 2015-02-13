ccea034175a5d4b19fdfe50faaa2feaa04663b5c
jmhohl13 <hohljm@rose-hulman.edu>
2015-02-13 08:36:28 -0500

    Path things
    oogaboogaboogah

--- Project Code/src/Path.java
+++ Project Code/src/Path.java
@@ -1,8 +1,39 @@
+import java.awt.Point;
+import java.util.ArrayList;
+
 /**
  * things are gonna change around here son   Prepare yourself
  * @author hohljm
  *
  */
 public class Path {
+	private double distanceEstimate;
+	private Node start;
+	private Node dest;
+	private ArrayList<Node>neighbors;
+	
+	public Path(Node starter, Node goal){
+		distanceEstimate=estimate();
+		start=starter;
+		dest=goal;
+		neighbors=start.getNeighbors();
+		
+	}
+	private double estimate(){
+		Point a=start.getCoord();
+		Point b= dest.getCoord();
+		return a.distance(b);
+		
+	}
+	public void createPaths(){
+		for(Node p:neighbors){
+			
+		}
+	
+	}
+	public Node bestPath(){
+		return null;
+	}
+	
 
 }
