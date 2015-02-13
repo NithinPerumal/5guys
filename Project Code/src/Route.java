d3cbc513f35161a8a925487f11de4de6482f9046
jmhohl13 <hohljm@rose-hulman.edu>
2015-02-13 08:48:58 -0500

    for max
    for max

--- Project Code/src/Route.java
+++ Project Code/src/Route.java
@@ -1,4 +1,42 @@
+import java.awt.Point;
+import java.util.ArrayList;
 
+/**
+ * shits about to go down here bois
+ * @author hohljm
+ *
+ */
 public class Route {
+	private Node start;
+	private Node dest;
+	private double dEstimate;
+	private ArrayList<Node>neighbors;
+	
+	
+	public Route(Node starter, Node finish){		
+		start=starter;
+		dest=finish;
+		neighbors=start.getNeighbors();
+		dEstimate=estimate();
+	}
+	public void createPaths(){
+		for(Node p:neighbors){
+			
+		}
+	
+	}
+	private double estimate(){
+		Point a=start.getCoord();
+		Point b= dest.getCoord();
+		return a.distance(b);
+		
+	}
+	public Node bestPath(){
+		return null;
+	}
+	
+	public double getEstimate(){
+		return this.dEstimate;
+	}
 
 }
