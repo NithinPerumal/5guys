eb41593c72f758c627ec6f75d12aafa5768dfe2c
Sam Weiskittel <samweiskittel@gmail.com>
2015-02-17 08:52:16 -0500

    Added structure for Action Listeners and confirmed the menu items work.

--- Project Code/src/topBar.java
+++ Project Code/src/topBar.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JMenu;
@@ -21,11 +22,46 @@ public class topBar {
 		JMenuItem itemm3 = new JMenuItem("City");
 		JMenuItem itemm32 = new JMenuItem("Path");
 		
-		menu.addActionListener(new ActionListener());
-		menu2.addActionListener(new ActionListener());
-		item.addActionListener(new ActionListener());
-		itemm3.addActionListener(new ActionListener());
-		itemm32.addActionListener(new ActionListener());
+		ActionListener menuListener = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//add the home sidebar to the frame and redraw
+				System.out.println("Home");
+			}
+		};
+		
+		ActionListener menu2Listener = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//add the trip planner sidebar to the frame and redraw
+				System.out.println("TripPlanner");
+			}
+		};
+		
+		ActionListener itemListener = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//add the hello sidebar to the frame and redraw
+				System.out.println("Hello");
+			}
+		};
+		
+		ActionListener itemm3Listener = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//add the city sidebar to the frame and redraw
+				System.out.println("City");
+			}
+		};
+		
+		ActionListener itemm32Listener = new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				//add the path sidebar to the frame and redraw
+				System.out.println("Path");
+			}
+		};
+		
+		menu.addActionListener(menuListener);
+		menu2.addActionListener(menu2Listener);
+		item.addActionListener(itemListener);
+		itemm3.addActionListener(itemm3Listener);
+		itemm32.addActionListener(itemm32Listener);
 		
 		menu.add(item);
 		menu3.add(itemm3);
