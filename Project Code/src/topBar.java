b2a11589c01defeb12b6bdc671737673e8b6acdb
Sam Weiskittel <samweiskittel@gmail.com>
2015-02-17 08:09:43 -0500

    Added new files to work on making the top buttons work. Finally got git to work.

--- Project Code/src/topBar.java
+++ Project Code/src/topBar.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.MenuItem;
+import java.awt.event.ActionListener;
 
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
@@ -19,6 +20,13 @@ public class topBar {
 		JMenuItem item = new JMenuItem("hello");
 		JMenuItem itemm3 = new JMenuItem("City");
 		JMenuItem itemm32 = new JMenuItem("Path");
+		
+		menu.addActionListener(new ActionListener());
+		menu2.addActionListener(new ActionListener());
+		item.addActionListener(new ActionListener());
+		itemm3.addActionListener(new ActionListener());
+		itemm32.addActionListener(new ActionListener());
+		
 		menu.add(item);
 		menu3.add(itemm3);
 		menu3.add(itemm32);
@@ -26,6 +34,8 @@ public class topBar {
 		menubar.add(menu2);
 		menubar.add(menu3);
 		menubar.setPreferredSize(new Dimension(1000, 20));
+		
+		
 	}
 
 }
