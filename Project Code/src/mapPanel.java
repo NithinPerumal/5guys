6a12339e41bfc57bf8261e66035f2975dd05c2b5
Tim Smith <timothys740@gmail.com>
2015-02-19 22:49:12 -0500

    updated map panel

--- Project Code/src/mapPanel.java
+++ Project Code/src/mapPanel.java
@@ -11,6 +11,8 @@ import javax.swing.JPanel;
 public class mapPanel extends JComponent {
 	ArrayList<Node> list;
 	Graphics2D g2;
+	Node select1;
+	Node select2;
 
 	public mapPanel(ArrayList<Node> cities) {
 		mouseListener mouse = new mouseListener();
@@ -26,6 +28,8 @@ public class mapPanel extends JComponent {
 		g.fillRect(0, 0, getWidth(), getHeight());
 		for (int i=0; i<list.size(); i++){
 			list.get(i).draw(g2, 15);
+			System.out.println(i);
+			g.drawString(list.get(i).name, (int)list.get(i).getXCoord(), (int)list.get(i).getYCoord() + 30);
 		}
 	}
 
@@ -34,12 +38,30 @@ public class mapPanel extends JComponent {
 		public void mouseClicked(MouseEvent e) {
 			int x = e.getLocationOnScreen().x;
 			int y = e.getLocationOnScreen().y;
+			int unselect = 1;
 			for (int i=0; i<list.size(); i++){
 			if (list.get(i).getXCoord() + 30 > x
 					&& list.get(i).getXCoord()  < x
 					&& list.get(i).getYCoord() + 70 > y
 					&& list.get(i).getYCoord() +50 < y) {
-				list.get(i).setColor(g2, Color.BLUE);
+				if (select1 != null && list.get(i) == select1){
+					select1 = null;
+					list.get(i).setColor(g2, Color.BLACK);
+					unselect = 0;
+				}
+				if (select2 != null && list.get(i) == select2){
+					select2 = null;
+					list.get(i).setColor(g2, Color.BLACK);
+					unselect = 0;
+				}
+				if (select1 == null && unselect == 1){
+					select1 = list.get(i);
+					list.get(i).setColor(g2, Color.BLUE);
+				}
+				if (select2 == null && select1 != list.get(i)&& unselect == 1){
+					select2 = list.get(i);
+					list.get(i).setColor(g2, Color.YELLOW);
+				}
 			}
 			}
 			repaint();
